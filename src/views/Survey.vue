1bc9d25a64f257cffc767e7964c95f09d84067cb
CHRISTOPHER KELLY <chris@nourishbalancethrive.com>
2021-01-02 10:30:37 -0800

    messing around with loading surveys

--- src/views/Survey.vue
+++ src/views/Survey.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="container">
-    <!-- If you want to hide survey, comment the lines below -->
-    <h2>SurveyJS Library - a sample survey below</h2>
     <survey :survey="survey"></survey>
   </div>
 </template>
 
 <script>
+const axios = require("axios").default;
 import * as SurveyVue from "survey-vue";
 import "bootstrap/dist/css/bootstrap.css";
 var Survey = SurveyVue.Survey;
@@ -31,152 +30,36 @@ customWidget(SurveyVue);
 
 SurveyVue.Serializer.addProperty("question", "tag:number");
 
+const API_URL = "/api/survey/";
+
 export default {
   components: {
-    Survey
+    Survey,
   },
   data() {
-    var json = {
-      title: "Product Feedback Survey Example",
-      showProgressBar: "top",
-      pages: [
-        {
-          questions: [
-            {
-              name: "date",
-              type: "datepicker",
-              inputType: "date",
-              title: "Your favorite date:",
-              dateFormat: "mm/dd/yy",
-              isRequired: true
-            },
-            {
-              type: "matrix",
-              name: "Quality",
-              title:
-                "Please indicate if you agree or disagree with the following statements",
-              columns: [
-                {
-                  value: 1,
-                  text: "Strongly Disagree"
-                },
-                {
-                  value: 2,
-                  text: "Disagree"
-                },
-                {
-                  value: 3,
-                  text: "Neutral"
-                },
-                {
-                  value: 4,
-                  text: "Agree"
-                },
-                {
-                  value: 5,
-                  text: "Strongly Agree"
-                }
-              ],
-              rows: [
-                {
-                  value: "affordable",
-                  text: "Product is affordable"
-                },
-                {
-                  value: "does what it claims",
-                  text: "Product does what it claims"
-                },
-                {
-                  value: "better then others",
-                  text: "Product is better than other products on the market"
-                },
-                {
-                  value: "easy to use",
-                  text: "Product is easy to use"
-                }
-              ]
-            },
-            {
-              type: "rating",
-              name: "satisfaction",
-              title: "How satisfied are you with the Product?",
-              mininumRateDescription: "Not Satisfied",
-              maximumRateDescription: "Completely satisfied"
-            },
-            {
-              type: "rating",
-              name: "recommend friends",
-              visibleIf: "{satisfaction} > 3",
-              title:
-                "How likely are you to recommend the Product to a friend or co-worker?",
-              mininumRateDescription: "Will not recommend",
-              maximumRateDescription: "I will recommend"
-            },
-            {
-              type: "comment",
-              name: "suggestions",
-              title: "What would make you more satisfied with the Product?"
-            }
-          ]
-        },
-        {
-          questions: [
-            {
-              type: "radiogroup",
-              name: "price to competitors",
-              title: "Compared to our competitors, do you feel the Product is",
-              choices: [
-                "Less expensive",
-                "Priced about the same",
-                "More expensive",
-                "Not sure"
-              ]
-            },
-            {
-              type: "radiogroup",
-              name: "price",
-              title: "Do you feel our current price is merited by our product?",
-              choices: [
-                "correct|Yes, the price is about right",
-                "low|No, the price is too low for your product",
-                "high|No, the price is too high for your product"
-              ]
-            },
-            {
-              type: "multipletext",
-              name: "pricelimit",
-              title: "What is the... ",
-              items: [
-                {
-                  name: "mostamount",
-                  title:
-                    "Most amount you would every pay for a product like ours"
-                },
-                {
-                  name: "leastamount",
-                  title: "The least amount you would feel comfortable paying"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          questions: [
-            {
-              type: "text",
-              name: "email",
-              title:
-                'Thank you for taking our survey. Please enter your email address, then press the "Submit" button.'
-            }
-          ]
-        }
-      ]
-    };
-    var model = new SurveyVue.Model(json);
     return {
-      survey: model
+      survey: new SurveyVue.Model(),
+      surveyName: "",
     };
-  }
+  },
+  mounted() {
+    const setSurvey = (response) => {
+      console.log(response);
+      this.surveyName = response.data.name;
+      this.survey = new SurveyVue.Model(response.data);
+      this.survey.onComplete.add(onSurveyComplete);
+    };
+    const onSurveyComplete = (sender, options) => {
+      axios
+        .post(`${API_URL}/${this.surveyName}/post`, sender.data)
+        .then(setSurvey)
+        .catch(console.log);
+    };
+    axios
+      .get(`${API_URL}/${this.surveyName}`)
+      .then(setSurvey)
+      .catch(console.log);
+  },
 };
 </script>
 
